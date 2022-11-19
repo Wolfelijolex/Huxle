267d5fe90b2bcfaac6ccd80b1af9af63f24f97e4
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2022-11-19 21:46:22 +0100

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style scoped lang="scss"></style>
