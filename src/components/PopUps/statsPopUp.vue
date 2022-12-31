e7576f3750d896cdd54aee9746c8abb951943512
Felix s1910238061 <s2210629018@fhooe.at>
2022-12-31 10:57:32 +0100

    Updated Share Button on Stats Panel

--- src/components/PopUps/statsPopUp.vue
+++ src/components/PopUps/statsPopUp.vue
@@ -13,8 +13,13 @@
               <div v-else-if="item.state === 'wrong-pos'" class="gridItemWrongPos">{{ item.char }}</div>
               <div v-else-if="item.state === 'correct'" class="gridItemCorrect">{{ item.char }}</div>
             </div>
+            <div v-for = "i in ((gameState.allTries.length - 30) *-1)" :key="i">
+              <div class = "gridItemFresh"></div>
+            </div>
+          </div>
+        <div class="shareButtonContainer">
+        <button class="shareButton" @click="copyToClipboard()">{{ clipBoardButtonText.value }}</button>
         </div>
-          <button class="shareButton" @click="copyToClipboard()">{{ clipBoardButtonText.value }}</button>
         </div>
       </div>
     </div>
@@ -62,12 +67,12 @@ gameState.allTries[1].state
 
 .gridContainer {
   /* center the item */
-  @apply flex flex-col justify-center items-center w-full h-full;
+  @apply flex flex-col justify-center items-center w-full h-full pb-6;
 
 }
 
 .gridItemFresh {
-  @apply m-1 w-auto h-12 font-bold text-white flex flex-col justify-center items-center bg-gray-500 rounded-lg;
+  @apply m-1 w-auto h-12 font-bold text-white flex flex-col justify-center items-center bg-gray-100 rounded-lg;
 }
 .gridItemNotIncluded {
   @apply  m-1 w-auto h-12 font-bold text-white flex flex-col justify-center items-center bg-gray-500 rounded-lg;
@@ -93,7 +98,7 @@ gameState.allTries[1].state
 }
 
 .closeButton {
-  @apply absolute text-center select-none duration-200 top-0 m-2 right-0 w-8 h-8 text-xl bg-gray-400 text-white font-bold cursor-pointer rounded;
+  @apply absolute z-40 text-center duration-200 top-0 m-2 right-0 w-8 h-8 text-xl bg-red-400 text-white font-bold cursor-pointer rounded;
 }
 
 .closeButton:hover {
@@ -113,7 +118,10 @@ gameState.allTries[1].state
 }
 
 .shareButton {
-  @apply absolute duration-300 text-center duration-200 bottom-0 right-0 w-full h-10 text-2xl bg-gray-400 text-white font-bold cursor-pointer;
+  @apply font-bold text-lg duration-200 bg-slate-400 overflow-hidden rounded-xl w-full h-auto p-4 text-center select-none;
+}
+.shareButtonContainer {
+  @apply flex flex-col mt-4 mb-4 justify-center items-center landscape:w-2/4 portrait:w-3/4;
 }
 
 .shareButton:hover {
@@ -121,6 +129,7 @@ gameState.allTries[1].state
 }
 
 .shareButton:active {
-  @apply absolute bg-blue-600 w-full h-9;
+  @apply bg-blue-600;
 }
+
 </style>
