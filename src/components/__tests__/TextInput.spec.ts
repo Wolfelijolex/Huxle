d5b22800e3a62335c85af0297b7a1beac60f1f14
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2023-01-05 22:07:47 +0100

    add pinia testing and fix unit test

--- src/components/__tests__/TextInput.spec.ts
+++ src/components/__tests__/TextInput.spec.ts
@@ -1,18 +1,23 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vitest } from "vitest";
 import LanguageSwitcher from "../LanguageSwitcher.vue";
 import { mount } from "@vue/test-utils";
 import { i18n } from "../../i18n";
+import { createTestingPinia } from "@pinia/testing";
 
 describe("LanguageSwitcher.vue", () => {
   test("should render available locales", () => {
-    const wrapper = mount(LanguageSwitcher, { global: { plugins: [i18n] } });
+    const wrapper = mount(LanguageSwitcher, {
+      global: { plugins: [i18n, createTestingPinia({ createSpy: vitest.fn })] },
+    });
     const texts = wrapper.findAll("li").map((li) => li.text());
     expect(texts).toContain("Deutsch");
     expect(texts).toContain("English");
   });
 
   test("should change locale", () => {
-    const wrapper = mount(LanguageSwitcher, { global: { plugins: [i18n] } });
+    const wrapper = mount(LanguageSwitcher, {
+      global: { plugins: [i18n, createTestingPinia({ createSpy: vitest.fn })] },
+    });
     const german = wrapper.findAll("button").find((li) => li.text() === "Deutsch");
     const english = wrapper.findAll("button").find((li) => li.text() === "English");
     expect(german).toBeDefined();
