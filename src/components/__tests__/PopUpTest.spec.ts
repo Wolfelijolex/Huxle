b9006943603c00cab9624f9efa72a0798ecf1766
Alexander G <alexgaertner1@hotmail.com>
2023-01-06 13:07:27 +0100

    Create PopUpTest.spec.ts

---
+++ src/components/__tests__/PopUpTest.spec.ts
@@ -0,0 +1,42 @@
+import { describe, expect, test, vitest } from "vitest";
+import PopupComponentVue from "@/components/PopUps/BasePopupComponent.vue";
+import { mount } from "@vue/test-utils";
+import { wrap } from "module";
+
+describe("BasePopUpComponent.vue", () => {
+  test("should not render when false", () => {
+    const wrapper = mount(PopupComponentVue, {
+      propsData: {
+        showCloseButton: false,
+        showPopup: false,
+      },
+    });
+    const nocontent = wrapper.findAll("div");
+    expect(nocontent).toStrictEqual([]);
+  });
+  test("should render when true", () => {
+    const wrapper = mount(PopupComponentVue, {
+      propsData: {
+        showCloseButton: false,
+        showPopup: true,
+      },
+    });
+    const content = wrapper.findAll("div");
+    expect(content).not.toStrictEqual([]);
+  });
+  test("should render close button and emit event", () => { 
+    const wrapper = mount(PopupComponentVue, {
+      propsData: {
+        showCloseButton: true,
+        showPopup: true,
+      },
+    });
+    const close =  wrapper.findAll("div").find((div) => div.text() === "x");
+    expect(close).toBeDefined();
+    expect(close?.text()).toBe("x");
+    close?.trigger("click");
+    wrapper.vm.$nextTick(() => { 
+      expect(wrapper.emitted()).toHaveProperty("click");
+    });
+  });
+});
