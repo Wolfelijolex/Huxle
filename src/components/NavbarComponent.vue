f26e4ad8bbaa9c12887bf13d1ed8908ce860f30d
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2023-01-06 18:25:47 +0100

    move navbar to own component

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gray-800 text-white text-lg font-bold pr-6 pl-6">
+    <div class="container px-2 mx-auto">
+      <div class="py-3 flex flex-row justify-between align-middle">
+        <p class="text-white text-2xl font-bold cursor-pointer duration-200 p-2 hover:bg-gray-600 rounded-xl">
+          {{ $t("huxle") }}
+        </p>
+        <LanguageSwitcher />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import LanguageSwitcher from "./LanguageSwitcher.vue";
+</script>
